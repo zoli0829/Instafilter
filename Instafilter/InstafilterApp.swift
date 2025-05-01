b9bd0417dde2629a37d1b524ab59f5ecb0758805
zoli0829 <zoli0829@gmail.com>
2025-05-01 05:08:55 +0100

    Initial commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Zoltan Vegh on 01/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
