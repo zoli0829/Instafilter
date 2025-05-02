81e219cd2109747e5126c00b49af00e2008114cc
zoli0829 <zoli0829@gmail.com>
2025-05-02 06:06:48 +0100

    finished day 63

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -5,28 +5,17 @@
 //  Created by Zoltan Vegh on 01/05/2025.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingConfirmation = false
-    @State private var backgroundColor: Color = .white
+    
     
     var body: some View {
-        Button("Hello, world") {
-            showingConfirmation.toggle()
-        }
-        .frame(width: 300, height: 300)
-        .background(backgroundColor)
-        .confirmationDialog("Change Background", isPresented: $showingConfirmation) {
-            Button("Red") {backgroundColor = .red}
-            Button("Green") {backgroundColor = .green}
-            Button("Blue") {backgroundColor = .blue}
-            Button("Cancel", role: .cancel) {}
-        } message: {
-            Text("Select a new color")
-        }
-        }
+        
     }
+}
 
 #Preview {
     ContentView()
